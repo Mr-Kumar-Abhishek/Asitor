57c212416f18d9fe7ae1bdcc1458bf8bedafffdc
Abhishek Kumar <kumar.abhishek@techie.com>
2016-03-08 17:32:42 +0530

    set welcome splash widget to start in center

--- asitor.cpp
+++ asitor.cpp
@@ -1,12 +1,34 @@
 #include <QApplication>
+#include <QDesktopWidget>
 #include <QWidget>
 
 int main(int argc, char* argv[]) {
+	
+
+	int width = 250;
+	int height = 150;
+
+	int screen_width;
+	int screen_height;
+
+	int x,y; // for storing the center values
+
 	QApplication asitor(argc, argv);
 
-	QWidget splash;
+	QWidget splash; // splash widget to welcome the user
 
-	splash.resize(250, 150);
+	QDesktopWidget *our_desktop = QApplication::desktop(); // taking desktop as a desktop widget
+
+	screen_width = our_desktop->width(); // screen's width
+	screen_height = our_desktop->height(); // screen's height
+
+	// getting center values..
+
+	y = (screen_height - height ) / 2;
+	x = (screen_width - width ) / 2; 
+
+	splash.resize(width, height);
+	splash.move(x,y);
 	splash.setWindowTitle("Asitor");
 	splash.show();
 
