19dc4204ba29c383339c447040f4d9cee973aff4
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-04-30 02:25:21 +0530

    remove extra header files from asitor.h

--- asitor.h
+++ asitor.h
@@ -2,24 +2,7 @@
 
 #include <QApplication>
 #include <QDesktopWidget>
-#include <QWidget>
-#include <QPushButton>
 #include <QPixmap>
 #include <QSplashScreen>
-#include <QIcon>
-#include <QLabel>
-#include <QGridLayout>
 #include <QObject>
-#include <QDialog>
-#include <QFileDialog>
-#include <QComboBox>
-#include <QDir>
-#include <QLineEdit>
-#include <QMessageBox>
-#include <QMdiArea>
-#include <QMainWindow>
-#include <QtSql>
-#include <QMenu>
-#include <QMenuBar>
-#include <QStatusBar>
 
