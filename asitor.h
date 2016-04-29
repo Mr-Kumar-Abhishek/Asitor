068a8b1c6c06ad4d4238a611d052dfd420391a93
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-04-30 01:20:49 +0530

    Separated choose_db class to a different file

--- asitor.h
+++ asitor.h
@@ -57,31 +57,6 @@ class manage_db : public QMainWindow {
 		QMenu *help_menu;
 };
 
-class choose_db : public QWidget{
-
-	Q_OBJECT
-
-	public:
-		choose_db(QWidget *parent = 0);
-
-	private slots:
-		void on_okay();
-		void on_browse();
-		void on_cancel();
-
-	private:
-		QLabel* admin_email; 
-		QLabel* password;
-		QLabel* choose_dir;
-		QLineEdit *email_line; 
-		QLineEdit *pass_line;
-		QPushButton *obutton;
-		QPushButton *bbutton;
-		QPushButton *cbutton;
-		QLineEdit *filepath;
-		QLabel *open_db_file;
-		void check_user(QString db_file);
-};
 
 class new_db : public QWidget {
 	
---
+++ choose_db.cpp
@@ -0,0 +1,124 @@
+#include "choose_db.h"
+
+choose_db::choose_db(QWidget *parent):QWidget(parent) {
+	this->setWindowTitle("Open Database Connection");
+	this->setWindowFlags(Qt::WindowCloseButtonHint);
+	this->resize(550,120);
+	admin_email = new QLabel("Admin Email: ", this);
+	password = new QLabel("Password: ", this);
+	email_line = new QLineEdit(this);
+	pass_line = new QLineEdit(this);
+	pass_line->setEchoMode(QLineEdit::Password); 
+	obutton = new QPushButton("Okay", this);
+	bbutton = new QPushButton("Browse..", this);
+	cbutton = new QPushButton("Cancel", this);
+	filepath = new QLineEdit(this);
+	open_db_file = new QLabel("Database File:", this);
+
+	
+	QVBoxLayout *vbox = new QVBoxLayout(this);
+
+	QGridLayout *grid = new QGridLayout();
+	grid->addWidget(admin_email, 0,0);
+	grid->addWidget(email_line, 0, 1);
+	grid->addWidget(password, 1, 0);
+	grid->addWidget(pass_line, 1, 1);
+
+	vbox->addLayout(grid);
+
+	
+	QGridLayout* grid2 = new QGridLayout();
+	grid2->addWidget(open_db_file,0, 0);
+	grid2->addWidget(filepath, 0, 1);
+	grid2->addWidget(bbutton, 0 ,2);
+
+	
+	//vbox->addSpacing(15);
+	vbox->addLayout(grid2);
+
+	QHBoxLayout* hbox = new QHBoxLayout();
+	hbox->addWidget(obutton, 1, Qt::AlignRight);
+	hbox->addWidget(cbutton, 0);
+	vbox->addStretch(1);
+	vbox->addLayout(hbox);
+
+	setLayout(vbox);
+
+	connect(bbutton, SIGNAL(clicked()), this, SLOT(on_browse()));
+	connect(obutton, SIGNAL(clicked()), this, SLOT(on_okay()));
+	connect(cbutton, SIGNAL(clicked()), this, SLOT(on_cancel()));
+}
+
+void choose_db::on_cancel() {
+	this->parentWidget()->close();
+}
+
+void choose_db::on_okay() {
+	QString db_file = filepath->text();
+	QMessageBox* db_messages = new QMessageBox(this);
+	if(QFile(db_file).exists()) {
+		QFileInfo* db_file_info = new QFileInfo(db_file);
+		if (!db_file_info->isDir()) {
+			QString db_check = db_file.right(3);
+			if (QString::compare(db_check, ".db", Qt::CaseInsensitive) == 0) {
+				qint64 size = db_file_info->size();
+				if(size != 0) {
+					check_user(db_file);
+				}else{
+					db_messages->setText("something is fishy here .. not a proper database file.");
+					db_messages->exec();
+				}
+			}else {
+				db_messages->setText("Oops ... not a database file to work on...");
+				db_messages->exec();
+			}
+		}else {
+			db_messages->setText("oops, got a directory instead of a file ..");
+			db_messages->exec();
+		}
+	}else {
+		db_messages->setText("File or folder doesn't exist at the specified location.");
+		db_messages->exec();
+	}
+}
+
+void choose_db::check_user(QString db_file){
+	QSqlDatabase* db = new QSqlDatabase();
+	*db = QSqlDatabase::addDatabase("QSQLITE");
+	db->setDatabaseName(db_file);
+	QMessageBox* db_messages = new QMessageBox();
+	QString admin;
+	QString pass;
+	if(db->open()){
+		QSqlQuery query;
+    	QString queryString = "SELECT email, pass FROM user";
+ 		query.exec(queryString);
+ 		while (query.next()) {
+        	QSqlRecord record = query.record();
+        	admin =	record.value(0).toString();
+        	pass  = record.value(1).toString();
+    	}
+    	QString chk_admin = email_line->text();
+    	QString chk_pass =  pass_line->text();
+    	if(QString::compare(chk_admin, admin) == 0 && QString::compare(chk_pass, pass) == 0){
+    		db_messages->setText("Succesfully logged into the database.");
+    		db_messages->exec();
+    	/*	
+    		manage_db* asitor_main = new manage_db();
+			asitor_main->show();
+			this->hide(); */
+    	}else{
+    		db_messages->setText("Email and/or password is incorrect !");
+    		db_messages->exec();
+    	}
+		db->close();
+	}else{
+		db_messages->setText("Error occured, couldn't open database file.");
+		db_messages->exec();
+	}
+}
+
+void choose_db::on_browse() {
+    QString filename = QFileDialog::getOpenFileName(this,tr("Find Database Files"), QDir::homePath(), tr("Database files (*.db)"));
+    filepath->insert(filename);
+}
