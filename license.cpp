0728a39ea80227109698c1b5e8fca4769f6890bc
Abhishek Kumar <mr.kumar.abhishek@email.com>
2016-10-24 01:05:55 +0530

    add widgets for license window

--- license.cpp
+++ license.cpp
@@ -1,5 +1,16 @@
 #include "license.h"
 
 license_win::license_win(QWidget* parent):sub_win(parent){
+    
     this->setWindowTitle("LICENSE");
+    
+    license_text = new QLabel("This software is licensed under GNU GPL v3");
+    license_okay = new QPushButton("Okay");
+    QVBoxLayout* vbox = new QVBoxLayout(this);
+    
+    vbox->addWidget(license_text, 1);
+    vbox->addWidget(license_okay, 0, Qt::AlignRight);
+    setLayout(vbox);
+    
+    connect(license_okay, SIGNAL(clicked()), this, SLOT(close_me()));
 }
--- license.h
+++ license.h
@@ -14,6 +14,7 @@ class license_win : public sub_win {
         
         
     private:
+        QLabel* license_text;
         QPushButton* license_okay;
 
 };
